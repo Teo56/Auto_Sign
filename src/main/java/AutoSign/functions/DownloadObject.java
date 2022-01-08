a5ecc5ce6744e7abb7cc2bdda8ac3388b77239a0
fcenciarelli <48931441+fcenciarelli@users.noreply.github.com>
2022-01-08 16:47:30 +0000

    Update DownloadObject.java

--- src/main/java/AutoSign/functions/DownloadObject.java
+++ src/main/java/AutoSign/functions/DownloadObject.java
@@ -15,14 +15,18 @@ import java.nio.file.Paths;
 public class DownloadObject {
     public static void downloadObject(
             String projectId, String bucketName, String objectName, String destFilePath) throws IOException {
+        
+        System.out.print("dest file path is:" + destFilePath);
+        System.out.print("object name is:" + objectName);
+        System.out.print(bucketName);
 
         // Change the directory of the credentials to get access to the Database online
         Credentials credentials = GoogleCredentials
-                .fromStream(new FileInputStream("ServiceKey_GoogleCloud copy.json"));
+                .fromStream(new FileInputStream("src/main/resources/ServiceKey_GoogleCloud copy.json"));
 
         // This is to set the name of the project online
         Storage storage = StorageOptions.newBuilder().setCredentials(credentials)
-                .setProjectId("autosign-334513").build().getService();
+                .setProjectId("autosign-336618").build().getService();
 
         // THIS IS TO DOWNLOAD THE FILE FROM THE CLOUD
         Blob blob = storage.get(BlobId.of(bucketName, objectName));
