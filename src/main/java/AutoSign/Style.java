f88d206e3745a8d87b2713280249b61e716f6a90
fcenciarelli <fracenciarelli2000@gmail.com>
2022-01-13 16:08:54 +0100

    icon add and button change

---
+++ src/main/java/AutoSign/Style.java
@@ -0,0 +1,17 @@
+package AutoSign;
+
+public class Style {
+    private boolean dark;
+
+    public Style(boolean dark) {
+        this.dark = dark;
+    }
+
+    public boolean getStyle() {
+        return dark;
+    }
+
+    public void setDark(boolean dark) {
+        this.dark = dark;
+    }
+}
