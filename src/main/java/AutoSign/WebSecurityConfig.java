9ae7684092b9368472c7309a51f7fa2de49d97c9
fcenciarelli <fracenciarelli2000@gmail.com>
2022-01-13 21:37:58 +0100

    https fix

--- src/main/java/AutoSign/WebSecurityConfig.java
+++ src/main/java/AutoSign/WebSecurityConfig.java
@@ -10,7 +10,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-                .requiresSecure();
+                .antMatchers("/").requiresSecure();
+//                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+//                .requiresSecure();
+
     }
 }
