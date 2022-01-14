b49a872c1b77434d5f6770d22bee083f98b8a4df
fcenciarelli <48931441+fcenciarelli@users.noreply.github.com>
2022-01-14 23:17:22 +0000

    Update AutoSignController.java

--- src/main/java/AutoSign/AutoSignController.java
+++ src/main/java/AutoSign/AutoSignController.java
@@ -85,7 +85,7 @@ public class AutoSignController {
         model.addAttribute("home", home);
         System.out.println(home.getURL());
         home.sendURL(home.getURL());
-        String url = "jdbc:postgresql://" + "ec2-54-170-212-187.eu-west-1.compute.amazonaws.com"+ ':' + "5432" + "d9p4pv538aoe9l" + "?sslmode=require";
+        String url = "jdbc:postgresql://" + "ec2-54-170-212-187.eu-west-1.compute.amazonaws.com"+ ':' + "5432/" + "d9p4pv538aoe9l" + "?sslmode=require";
         //String url ="postgresql://nmcnudamsuzpvx:1a486086f1f7ece7c2c568408c62d36e23b78bbe38118c288e088f9be6042433@ec2-54-170-212-187.eu-west-1.compute.amazonaws.com:5432/d9p4pv538aoe9l";
         //String url = "jdbc:postgresql://localhost:5432/postgres";
         String user = "nmcnudamsuzpvx";
