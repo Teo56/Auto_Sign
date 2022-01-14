afc12dd8bc0717c82ac023d911391c962b46e588
Teo56 <77128181+Teo56@users.noreply.github.com>
2022-01-14 23:24:22 +0100

    Update PrintDbUrl.java

--- src/main/java/AutoSign/functions/PrintDbUrl.java
+++ src/main/java/AutoSign/functions/PrintDbUrl.java
@@ -10,13 +10,16 @@ public class PrintDbUrl {
     public static String printdburl(){
 
         // RETRIEVE INFORMATION FROM DATABASE
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "postgres";
-        String password = "Arius135";
+        String url ="postgres://nmcnudamsuzpvx:1a486086f1f7ece7c2c568408c62d36e23b78bbe38118c288e088f9be6042433@ec2-54-170-212-187.eu-west-1.compute.amazonaws.com:5432/d9p4pv538aoe9l";
+        //String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "nmcnudamsuzpvx";
+        //String user = "postgres";
+        String password = "1a486086f1f7ece7c2c568408c62d36e23b78bbe38118c288e088f9be6042433";
+        //String password = "Arius135";
         String str = "<div class=\"ex8\">" + "<br>" + "<li class=\"ex5\">" + "URL" + "</li>" + "<li class=\"ex6\">" + "TITLE" + "</li>" + "<li class=\"ex7\">" + "DURATION" + "</li>"  + "</br>" + "</div>";;
 
         try (Connection con = DriverManager.getConnection(url, user, password);
-             PreparedStatement pst = con.prepareStatement("SELECT * FROM videos");
+             PreparedStatement pst = con.prepareStatement("SELECT * FROM d9p4pv538aoe9l");
              ResultSet rs = pst.executeQuery()) {
 
             int i=0;
