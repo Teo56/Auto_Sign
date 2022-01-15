cf2de3e1c321539b279c45c4068ba383e716d47a
Teo56 <77128181+Teo56@users.noreply.github.com>
2022-01-15 15:38:57 +0100

    Update CheckVideoExist.java

--- src/main/java/AutoSign/functions/CheckVideoExist.java
+++ src/main/java/AutoSign/functions/CheckVideoExist.java
@@ -13,25 +13,30 @@ import java.io.IOException;
 
 public class CheckVideoExist {
 
+    // This method checks whether the translation video (recognised by its filename) is present in google cloud storage
+    // When that happens, it returns true
     public static boolean CheckVideoExists(String videoid) throws IOException {
         boolean exists;
         String videoID = videoid;
-        String vidpath = "/tmp/" + videoID + ".mp4";
-        String bucketName = "auto-sign-main";
+        String vidpath = "/tmp/" + videoID + ".mp4"; // What to look for in the cloud storage
+        String bucketName = "auto-sign-main"; // bucket of the cloud where to look into
 
+        // Credentials to access the cloud
         Credentials credentials = GoogleCredentials
                 .fromStream(new FileInputStream("google-credentials.json"));
 
+        // Access the cloud
         Storage storage = StorageOptions.newBuilder().setCredentials(credentials)
                 .setProjectId("autosign-336618").build().getService();
 
+        // Check if the video is there or not
         if (storage.get(BlobId.of(bucketName, vidpath)) != null) {
-            exists = true;
+            exists = true; // If video found exits = true
         } else {
-            exists = false;
+            exists = false; // If video not found exit = false
         }
 
-        return exists;
+        return exists; // returns the state of the search (found or not found)
     }
 
 
