196f33061530d97caa8d902e24284598a9458ae9
Teo56 <77128181+Teo56@users.noreply.github.com>
2022-01-15 15:59:52 +0100

    Update AutoSignControllerTests.java

--- src/test/java/AutoSign/AutoSignControllerTests.java
+++ src/test/java/AutoSign/AutoSignControllerTests.java
@@ -10,51 +10,54 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// This class contains the tests for the autosign controller methods
+// these mostly include mapping tests (checking the status of the response after the get/post mapping is done)
+
 @WebMvcTest
 public class AutoSignControllerTests {
 
     @Autowired
-    private MockMvc mockmvc;
+    private MockMvc mockmvc; // Creates a mock client to submit the requests
 
 
     @Test
     void HomeTest() throws Exception {
         mockmvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
-                .andReturn();
+                .andExpect(status().isOk()) // Checks that the status code of the sesponse is 200 (=OK -> request properly sent)
+                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8"))) // Checks that the content associated to that requests is of type html
+                .andReturn(); // Defines return value
     }
 
     @Test
     void ContactUsTest() throws Exception {
         mockmvc.perform(get("/contactus"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
-                .andReturn();
+                .andExpect(status().isOk()) // Checks that the status code of the sesponse is 200 (=OK -> request properly sent)
+                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8"))) // Checks that the content associated to that requests is of type html
+                .andReturn(); // Defines return value
     }
 
     @Test
     void HistoryTest() throws Exception {
         mockmvc.perform(get("/history"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
-                .andReturn();
+                .andExpect(status().isOk()) // Checks that the status code of the sesponse is 200 (=OK -> request properly sent)
+                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8"))) // Checks that the content associated to that requests is of type html
+                .andReturn(); // Defines return value
     }
 
     @Test
     void AboutUsTest() throws Exception {
         mockmvc.perform(get("/aboutus"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
-                .andReturn();
+                .andExpect(status().isOk()) // Checks that the status code of the sesponse is 200 (=OK -> request properly sent)
+                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8"))) // Checks that the content associated to that requests is of type html
+                .andReturn(); // Defines return value
     }
 
     @Test
     void DonateTest() throws Exception {
         mockmvc.perform(get("/donate"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8")))
-                .andReturn();
+                .andExpect(status().isOk()) // Checks that the status code of the sesponse is 200 (=OK -> request properly sent)
+                .andExpect(content().contentType(MediaType.valueOf("text/html;charset=UTF-8"))) // Checks that the content associated to that requests is of type html
+                .andReturn(); // Defines return value
     }
 
 
