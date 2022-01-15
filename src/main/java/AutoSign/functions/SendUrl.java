b71a4e8b55428618d981bbb482887f9c02b8a2cb
Teo56 <77128181+Teo56@users.noreply.github.com>
2022-01-15 15:23:24 +0100

    Update SendUrl.java

--- src/main/java/AutoSign/functions/SendUrl.java
+++ src/main/java/AutoSign/functions/SendUrl.java
@@ -9,17 +9,27 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SendUrl {
+    
+    // THIS METHOD IS RUN WITHIN THE SUBMITURL METHOD, AND IT ALLOWS TO SEND THE SUBMITTED URL TO THE PYTHON SERVER FOR VIDEO TRANSLATION
     public static void sendurl(String url) throws JSONException {
 
+        // Creates new json object
         JSONObject json = new JSONObject();
+        // Pass the url to the json object
         json.put("url", url);
 
+        // create HttpClient to submit the request
         HttpClient httpClient = HttpClientBuilder.create().build();
         try {
+            // Create post request to python server
             HttpPost request = new HttpPost("https://python-microservice.herokuapp.com/download");
-            StringEntity params = new StringEntity(json.toString());
+            // Assign json obect as parameter of the request
+            StringEntity params = new StringEntity(json.toString()); 
+            // Add header to the request
             request.addHeader("content-type", "application/json");
+            // Set parameter(json object) as entity of the request
             request.setEntity(params);
+            // Execute post request and get the response
             HttpResponse response = httpClient.execute(request);
         } catch (Exception ex) {
         }
