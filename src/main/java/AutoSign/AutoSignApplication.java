2da36b1b5e970f92f2606a535c3fc05b415c1204
Teo56 <77128181+Teo56@users.noreply.github.com>
2022-01-15 14:23:43 +0100

    Update AutoSignApplication.java

--- src/main/java/AutoSign/AutoSignApplication.java
+++ src/main/java/AutoSign/AutoSignApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutoSignApplication {
-
+    
+    // The main method of this class allows to start the whole web app
     public static void main(String[] args) {
         SpringApplication.run(AutoSignApplication.class, args);
     }
