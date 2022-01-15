3b8aee3c0e33519adaf1b058fdb111bf7d5eb4ef
Teo56 <77128181+Teo56@users.noreply.github.com>
2022-01-15 16:03:45 +0100

    Update HomeViewTests.java

--- src/test/java/AutoSign/HomeViewTests.java
+++ src/test/java/AutoSign/HomeViewTests.java
@@ -3,24 +3,24 @@ package AutoSign;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-
+// Class to test the get and set methods of HomeView
 public class HomeViewTests {
 
     @Test
     void getURLTest() {
-        HomeView home = new HomeView();
-        home.setURL("https://www.youtube.com/watch?v=8W7ec-FoUJ0");
-        String result_test = home.getURL();
+        HomeView home = new HomeView(); // Create new HomeView
+        home.setURL("https://www.youtube.com/watch?v=8W7ec-FoUJ0"); // Set url
+        String result_test = home.getURL(); // Get url
 
-        Assert.assertEquals("https://www.youtube.com/watch?v=8W7ec-FoUJ0", result_test);
+        Assert.assertEquals("https://www.youtube.com/watch?v=8W7ec-FoUJ0", result_test); // Check the gotten url is the same as the expected
     }
 
     @Test
     void setURLTest() {
-        HomeView home = new HomeView();
-        home.setURL("https://www.youtube.com/watch?v=B-cMJ1qJ0kQ&t=4s");
+        HomeView home = new HomeView(); // Create HomeView
+        home.setURL("https://www.youtube.com/watch?v=B-cMJ1qJ0kQ&t=4s"); // Set url
 
-        Assert.assertEquals("https://www.youtube.com/watch?v=B-cMJ1qJ0kQ&t=4s", home.getURL());
+        Assert.assertEquals("https://www.youtube.com/watch?v=B-cMJ1qJ0kQ&t=4s", home.getURL()); // Check the set url is the same as the expected by get
     }
 
 }
