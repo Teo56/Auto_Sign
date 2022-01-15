e12cfd07ff70b4e74dc47f4b8b6dfbc81983d043
fcenciarelli <48931441+fcenciarelli@users.noreply.github.com>
2022-01-15 14:35:28 +0000

    Update StartVideo.java

--- src/main/java/AutoSign/functions/StartVideo.java
+++ src/main/java/AutoSign/functions/StartVideo.java
@@ -5,11 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+// Class to pass the video url to the final result page where the video is shown
+// It is needed to go from the Loading page to the Result Page
 public class StartVideo {
     public static void startVideo(@RequestParam("url") String url, @ModelAttribute HomeView home, Model model) {
-        HomeView homeee = new HomeView();
-        homeee.setURL(url);
-        model.addAttribute("homeee", homeee);
+        HomeView homeee = new HomeView(); //create a new object of type HomeView()
+        homeee.setURL(url); // pass the URL to it
+        model.addAttribute("homeee", homeee); // inject model into the method
         System.out.print(homeee.getURL());
     }
 
